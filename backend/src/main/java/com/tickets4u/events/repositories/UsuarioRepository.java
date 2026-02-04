b1a65dcd9f6bb6078ebd4c3939a07ca0b408a4bd
noe-al <n.alba@pro2fp.es>
2026-02-04 16:58:56 +0100

    Delete backend directory

--- backend/src/main/java/com/tickets4u/events/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tickets4u.events.repositories;
-
-import com.tickets4u.models.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
